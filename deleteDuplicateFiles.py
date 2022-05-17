90bbda8e422aa39503621fa73931c7c619e7710c
Christel1234 <christel.jansenvanrensburg@yoco.com>
2022-05-17 10:47:10 +0200

    challenge 2 fixes

--- deleteDuplicateFiles.py
+++ deleteDuplicateFiles.py
@@ -3,17 +3,22 @@ from tkinter import Tk
 import os
 import hashlib
 
-Tk().withdraw()
-path = askdirectory(title="Select folder")
-dirs = os.walk(path)
-newFiles = []
 
-for folder, subfolder, files in dirs:
-    for file in files:
-        filepath = os.path.join(folder, file)
-        hash = hashlib.md5(open(filepath, "rb").read()).hexdigest()
-        if hash in newFiles:
-            os.remove(filepath)
-            print(f"{filepath} has been deleted")
-        else:
-            newFiles.append(hash)
+def deleteDuplicateFiles():
+    Tk().withdraw()
+    path = askdirectory(title="Select folder")
+    dirs = os.walk(path)
+    newFiles = []
+    for folder, subfolder, files in dirs:
+        for file in files:
+            filepath = os.path.join(folder, file)
+            hash = hashlib.md5(open(filepath, "rb").read()).hexdigest()
+            if hash in newFiles:
+                os.remove(filepath)
+                print(f"{filepath} has been deleted")
+            else:
+                newFiles.append(hash)
+
+
+if __name__ == "__main__":
+    deleteDuplicateFiles()
